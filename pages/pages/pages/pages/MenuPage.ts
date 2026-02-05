94cf4c9e551ffb34afdddcf31839dec5dd5e30ca
Sadia <sadiamim646@gmail.com>
2026-02-05 10:30:52 +0600

    Update MenuPage.ts

--- pages/pages/pages/pages/MenuPage.ts
+++ pages/pages/pages/pages/MenuPage.ts
@@ -1,1 +1,16 @@
+import { Page } from '@playwright/test';
+
+export class MenuPage {
+  constructor(private page: Page) {}
+
+  async resetAppState() {
+    await this.page.click('#react-burger-menu-btn');
+    await this.page.click('#reset_sidebar_link');
+  }
+
+  async logout() {
+    await this.page.click('#react-burger-menu-btn');
+    await this.page.click('#logout_sidebar_link');
+  }
+}
 
