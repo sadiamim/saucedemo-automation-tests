803aeffcff468191fefebb43172eee340849f4bd
Sadia <sadiamim646@gmail.com>
2026-02-05 10:30:24 +0600

    Update CheckoutPage.ts

--- pages/pages/pages/CheckoutPage.ts
+++ pages/pages/pages/CheckoutPage.ts
@@ -1,1 +1,21 @@
+import { Page } from '@playwright/test';
+
+export class CheckoutPage {
+  constructor(private page: Page) {}
+
+  async fillInformation() {
+    await this.page.fill('#first-name', 'Test');
+    await this.page.fill('#last-name', 'User');
+    await this.page.fill('#postal-code', '12345');
+    await this.page.click('#continue');
+  }
+
+  async finishOrder() {
+    await this.page.click('#finish');
+  }
+
+  async getSuccessMessage() {
+    return this.page.locator('.complete-header').textContent();
+  }
+}
 
