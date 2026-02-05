81a4ceada687416b23f5cb75ef445411c240d85e
Sadia <sadiamim646@gmail.com>
2026-02-05 10:29:16 +0600

    Update InventoryPage.ts

--- pages/pages/InventoryPage.ts
+++ pages/pages/InventoryPage.ts
@@ -1,1 +1,25 @@
+import { Page } from '@playwright/test';
+
+export class InventoryPage {
+  constructor(private page: Page) {}
+
+  async addFirstItemToCart() {
+    await this.page.locator('.inventory_item button').first().click();
+  }
+
+  async sortBy(option: string) {
+    await this.page.selectOption('.product_sort_container', option);
+  }
+
+  async addItems(count: number) {
+    const buttons = this.page.locator('.inventory_item button');
+    for (let i = 0; i < count; i++) {
+      await buttons.nth(i).click();
+    }
+  }
+
+  async goToCart() {
+    await this.page.click('.shopping_cart_link');
+  }
+}
 
