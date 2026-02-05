03c855949909263bd6397f386bc3532b92412eb2
Sadia <sadiamim646@gmail.com>
2026-02-05 10:28:26 +0600

    Update LoginPage.ts

--- pages/LoginPage.ts
+++ pages/LoginPage.ts
@@ -1,1 +1,15 @@
-export class LoginPage {}
+import { Page } from '@playwright/test';
+
+export class LoginPage {
+  constructor(private page: Page) {}
+
+  async login(username: string, password: string) {
+    await this.page.fill('#user-name', username);
+    await this.page.fill('#password', password);
+    await this.page.click('#login-button');
+  }
+
+  async getErrorMessage() {
+    return this.page.locator('[data-test="error"]').textContent();
+  }
+}
