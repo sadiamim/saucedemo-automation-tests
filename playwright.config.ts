61c79ee2c228e64bf8dfa92fb4d60b77b392cf41
Sadia <sadiamim646@gmail.com>
2026-02-05 10:27:38 +0600

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,1 +1,19 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  retries: 0,
+  workers: 1,
+  reporter: [
+    ['list'],
+    ['allure-playwright']
+  ],
+  use: {
+    baseURL: 'https://www.saucedemo.com',
+    browserName: 'chromium',
+    headless: true,
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure'
+  }
+});
 
