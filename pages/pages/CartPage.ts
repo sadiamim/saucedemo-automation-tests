3b4dd1e468f645ca05caf40fda38293f45871071
Sadia <sadiamim646@gmail.com>
2026-02-05 10:29:55 +0600

    Update CartPage.ts

--- pages/pages/CartPage.ts
+++ pages/pages/CartPage.ts
@@ -1,1 +1,14 @@
+import { Page } from '@playwright/test';
+
+export class CartPage {
+  constructor(private page: Page) {}
+
+  async checkout() {
+    await this.page.click('#checkout');
+  }
+
+  async getProductNames() {
+    return this.page.locator('.inventory_item_name').allTextContents();
+  }
+}
 
