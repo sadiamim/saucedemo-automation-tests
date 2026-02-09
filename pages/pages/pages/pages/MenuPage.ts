904461a49792f6ab221108b05de92fcc9c99cef3
Sadia Sultana <sadiamim646@gmail.com>
2026-02-09 11:03:37 +0600

    Update MenuPage.ts

--- pages/pages/pages/pages/MenuPage.ts
+++ pages/pages/pages/pages/MenuPage.ts
@@ -5,6 +5,8 @@ export class MenuPage {
 
   async resetAppState() {
     await this.page.click('#react-burger-menu-btn');
+    await this.page.waitForSelector('#logout_sidebar_link', { state: 'visible' });
+    await this.page.click('#logout_sidebar_link');
     await this.page.click('#reset_sidebar_link');
   }
 
