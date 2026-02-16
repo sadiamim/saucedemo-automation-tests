afa474a91a9c8b5ca67fea0ef5a70badfd79c9d5
Sadia Sultana <sadiamim646@gmail.com>
2026-02-16 16:37:35 +0600

    MenuPage.ts

--- pages/pages/pages/pages/MenuPage.ts
+++ pages/pages/pages/pages/MenuPage.ts
@@ -5,12 +5,16 @@ export class MenuPage {
 
   async resetAppState() {
     await this.page.click('#react-burger-menu-btn');
+    await this.page.waitForSelector('#logout_sidebar_link', { state: 'visible' });
+    await this.page.click('#logout_sidebar_link');
     await this.page.click('#reset_sidebar_link');
   }
 
   async logout() {
-    await this.page.click('#react-burger-menu-btn');
-    await this.page.click('#logout_sidebar_link');
-  }
+  await this.page.locator('#react-burger-menu-btn').click({ force: true });
+  await this.page.waitForSelector('#logout_sidebar_link', { state: 'visible' });
+  await this.page.click('#logout_sidebar_link');
+}
+
 }
 
